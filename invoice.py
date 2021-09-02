29b294ec04e36be7af92943fd47a87962767881b
Raimon Esteve <raimonesteve@gmail.com>
2021-09-02 10:05:02 +0200

    FIX check maturity dates in scenario

--- invoice.py
+++ invoice.py
@@ -1,4 +1,4 @@
-# This file is part account_payment_days module for Tryton.
+# This file is part account_payment_holidays module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from functools import wraps
